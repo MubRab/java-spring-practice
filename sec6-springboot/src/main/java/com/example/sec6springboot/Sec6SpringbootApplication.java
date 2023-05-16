db4fea0f969a3505ca2680abe0c40f1b52cd6478
MubRab <rab.mubtasim@gmail.com>
2023-05-16 11:21:22 +0200

    added sec6

---
+++ sec6-springboot/src/main/java/com/example/sec6springboot/Sec6SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sec6springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec6SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sec6SpringbootApplication.class, args);
+	}
+
+}
---
+++ sec6-springboot/src/test/java/com/example/sec6springboot/Sec6SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec6springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec6SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
