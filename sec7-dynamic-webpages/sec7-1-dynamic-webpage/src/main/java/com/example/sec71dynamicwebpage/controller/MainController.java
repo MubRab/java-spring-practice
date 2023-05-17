22033a5237329fb8edce785daea859733b880df4
MubRab <rab.mubtasim@gmail.com>
2023-05-17 12:54:53 +0200

    added parameters to sec7-1

--- sec7-dynamic-webpages/sec7-1-dynamic-webpage/src/main/java/com/example/sec71dynamicwebpage/controller/MainController.java
+++ sec7-dynamic-webpages/sec7-1-dynamic-webpage/src/main/java/com/example/sec71dynamicwebpage/controller/MainController.java
@@ -3,14 +3,15 @@ package com.example.sec71dynamicwebpage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String home(Model page) {
-        page.addAttribute("user", "John");
-        page.addAttribute("colour","red");
+    public String home(@RequestParam(defaultValue = "John") String user, @RequestParam(defaultValue = "red") String colour, Model page) {
+        page.addAttribute("user", user);
+        page.addAttribute("colour",colour);
         return "home.html";
     }
 }
