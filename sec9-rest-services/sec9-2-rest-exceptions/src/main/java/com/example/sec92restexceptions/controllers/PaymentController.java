5eca07757c5eea57f5d5f8303c0f8f8db89df7bb
MubRab <rab.mubtasim@gmail.com>
2023-05-25 07:19:26 +0200

    updated sec9-2

--- sec9-rest-services/sec9-2-rest-exceptions/src/main/java/com/example/sec92restexceptions/controllers/PaymentController.java
+++ sec9-rest-services/sec9-2-rest-exceptions/src/main/java/com/example/sec92restexceptions/controllers/PaymentController.java
@@ -7,19 +7,36 @@ import com.example.sec92restexceptions.services.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
     private final PaymentService paymentService;
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
-    @GetMapping("/pay")
+    @PostMapping("/pay")
     public ResponseEntity<?> makePayment() {
         PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
+    }
+
+    @PostMapping("/receive")
+    public ResponseEntity<?> receivePayment(@RequestBody PaymentDetails paymentDetails) {
+        Currency rands = Currency.getInstance(new Locale("en", "ZA"));
+        logger.info("Received amount: " + NumberFormat.getCurrencyInstance().format(paymentDetails.getAmount()));
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
